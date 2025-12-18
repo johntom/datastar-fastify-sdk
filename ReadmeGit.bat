1c3e25bf93b0b18e29efa31b63a1d567ab433dac
johntom <jrt@gtz.com>
2025-12-18 07:49:14 -0500

    4th commit

---
+++ ReadmeGit.bat
@@ -0,0 +1,9 @@
+git add .
+pause
+git commit -m "4th commit"
+pause
+git push
+
+
+@REM <body class="bg-white dark:bg-gray-900 text-lg max-w-xl mx-auto my-16">
+@REM        35 +  <body class="bg-black text-lg max-w-xl mx-auto my-16">
